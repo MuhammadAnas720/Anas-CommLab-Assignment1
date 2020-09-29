38aaa3070012764744e4ec546384f81ce2d8ef7b
NYUAD <nyuad@C2-LIB-AIR-17.local>
2020-09-29 17:07:30 +0400

    a

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-var figure = $(".video").hover(hoverVideo, hideVideo);
+window.onload = function() { //executes code after DOM loads
+ //--- select all <video> on the page
+ const vids = document.querySelectorAll('.thevideo')
+ // Loop over the selected elements and add event listeners
+ for (let i = 0; i < vids.length; i++) {
+   vids[i].addEventListener( 'mouseover', function(e) {
+     vids[i].play()
+     vids[i].style.width = "500px";
+     vids[i].style.left = "-25px";
 
-function hoverVideo(e) {
-  $('video', this).get(0).play();
-}
-
-function hideVideo(e) {
-  $('video', this).get(0).pause();
+   })
+   vids[i].addEventListener( 'mouseout', function(e) {
+     vids[i].pause()
+     vids[i].style.width = "465px";
+     vids[i].style.left = "-7px";
+   })
+ }
 }
